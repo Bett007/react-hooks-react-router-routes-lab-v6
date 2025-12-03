8d0763a242dc97753f8b8d2b6890827386e010d5
Bett007 <bettbrian022@gmail.com>
2025-12-03 19:05:45 +0300

    Handling errors

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+export default function ErrorPage() {
+  return (
+    <div>
+      <NavBar />
+      <main>
+        <h1>Oops! Something went wrong.</h1>
+      </main>
+    </div>
+  );
+}
