6cee41096c440800cb89833057b55d0cea4e88d1
Bett007 <bettbrian022@gmail.com>
2025-12-03 19:04:35 +0300

    Setting up Directors

--- src/pages/Directors.jsx
+++ src/pages/Directors.jsx
@@ -1,16 +1,24 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import NavBar from "../components/NavBar";
+import directors from "../data-directors";
 
-function Directors() {
+export default function Directors() {
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
+    <div>
+      <NavBar />
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directors.map((d) => (
+          <article key={d.name}>
+            <h2>{d.name}</h2>
+            <ul>
+              {d.movies.map((mv, i) => (
+                <li key={i}>{mv}</li>
+              ))}
+            </ul>
+          </article>
+        ))}
       </main>
-    </>
+    </div>
   );
-};
-
-export default Directors;
+}
