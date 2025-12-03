c3ed073292c76909ca71c502bd9cf2804ac40f8f
Bett007 <bettbrian022@gmail.com>
2025-12-03 19:06:39 +0300

    data.js

---
+++ src/data.js
@@ -0,0 +1,7 @@
+const movies = [
+  { id: 1, title: "Moonlight Drive", time: "120 min", genres: ["Drama", "Mystery"] },
+  { id: 2, title: "Sunrise Over Nairobi", time: "95 min", genres: ["Romance"] },
+  { id: 3, title: "Code: Origin", time: "110 min", genres: ["Sci-Fi"] },
+];
+
+export default movies;
