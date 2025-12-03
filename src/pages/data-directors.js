df669138e718b75d992227a1babb3f86932072e4
Bett007 <bettbrian022@gmail.com>
2025-12-03 19:07:29 +0300

    data for directors

---
+++ src/pages/data-directors.js
@@ -0,0 +1,6 @@
+const directors = [
+  { name: "Asha Mwangi", movies: ["Moonlight Drive", "Sunrise Over Nairobi"] },
+  { name: "Peter Otieno", movies: ["Code: Origin"] },
+];
+
+export default directors;
