433fbd9c8a8b85f337ac6bebdfca5ca3c96abf64
Bett007 <bettbrian022@gmail.com>
2025-12-03 19:03:33 +0300

    Home setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard";
+import movies from "../data";
 
-function Home() {
+export default function Home() {
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
+    <div>
+      <NavBar />
       <main>
-        {/* Info goes here! */}
+        <h1>Home Page</h1>
+        {movies.map((m) => (
+          <MovieCard key={m.id} movie={m} />
+        ))}
       </main>
-    </>
+    </div>
   );
-};
-
-export default Home;
+}
