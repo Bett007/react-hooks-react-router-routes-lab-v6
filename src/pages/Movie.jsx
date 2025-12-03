5aa46cc325c177af480a3663b0a69b43fee1af24
Bett007 <bettbrian022@gmail.com>
2025-12-03 19:04:05 +0300

    Movies.jsx

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,16 +1,30 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import movies from "../data";
 
-function Movie() {
+export default function Movie() {
+  const { id } = useParams();
+  const movie = movies.find((m) => String(m.id) === String(id));
+
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
+    <div>
+      <NavBar />
       <main>
-        {/* Movie info here! */}
+        {movie ? (
+          <article>
+            <h1>{movie.title}</h1>
+            <p>{movie.time}</p>
+            <div>
+              {movie.genres.map((g, idx) => (
+                <span key={idx}>{g} </span>
+              ))}
+            </div>
+          </article>
+        ) : (
+          <h1>Movie not found</h1>
+        )}
       </main>
-    </>
+    </div>
   );
-};
-
-export default Movie;
+}
